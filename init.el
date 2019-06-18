267686b5ecd5250ee1bbec99097c7641687fc1ce
casey <939896399@qq.com>
2019-06-18 21:55:24 +0800

    - ‘.#init.el’ / ‘init.el’

--- init.el
+++ init.el
@@ -67,6 +67,8 @@ values."
      ;; mcu
      ;; wiki
      graphviz
+     gnus
+     mu4e
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
