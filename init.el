596f42533e5655d274d49f0f68820b5c913249a1
tong <939896399@qq.com>
2019-09-06 15:19:35 +0800

    - ‘.#init.el’ / ‘init.el’

--- init.el
+++ init.el
@@ -70,6 +70,7 @@ values."
      graphviz
      mu4e
      gnus
+     protobuf
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
