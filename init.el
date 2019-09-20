8e64a6e1c5bfdbf0d7f1a7f989e05f7a7e9f7496
tong <939896399@qq.com>
2019-09-20 14:05:12 +0800

    / ‘init.el’

--- init.el
+++ init.el
@@ -71,6 +71,7 @@ values."
      mu4e
      gnus
      protobuf
+     gpu
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
