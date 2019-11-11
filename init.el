c59ea2c1900f6377c11cd2e26c4a57ad253cf60b
tong <939896399@qq.com>
2019-11-11 10:16:01 +0800

    / ‘init.el’

--- init.el
+++ init.el
@@ -77,8 +77,8 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(youdao-dictionary 
-                                      (eaf :location(recipe :fetcher github :repo "manateelazycat/emacs-application-framework")))
+   dotspacemacs-additional-packages '(youdao-dictionary eaf)
+
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
