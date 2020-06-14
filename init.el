1086977b9a09c5e7abaf847011255fe5992b7865
casey <939896399@qq.com>
2020-06-14 09:52:08 +0800

    / ‘init.el’

--- init.el
+++ init.el
@@ -547,7 +547,7 @@ same directory as the org-buffer and insert a link to this file."
 
   ;; (add-hook 'org-mode-hook (lambda () (yas-load-directory (expand-file-name "~/.spacemacs.d/snippets/."))))
 
-  (setq org-refile-targets (list (cons nil (cons :maxlevel 1))))
+  ;; (setq org-refile-targets (list (cons nil (cons :maxlevel 1))))
 
   (defun org-mode-my-init ()
     (define-key org-mode-map (kbd "×") (kbd "*"))
